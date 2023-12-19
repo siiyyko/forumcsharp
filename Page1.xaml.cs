ad316863a3b6d89233dd962e54a03e0542add80d
siiyyko <oleksii.blyzniuk.pz.2022@lpnu.ua>
2023-12-20 00:12:36 +0200

    Add project files.

---
+++ Page1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Windows.Controls;
+
+namespace forumcsharp
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+            string[] dataBase;
+            string[] LoginAndPassword;
+
+            if (LoginTextBox.Text != "" && PasswordTextBox.Password != "")
+            {
+                dataBase = File.ReadAllText("database.txt").Split("\n");
+                foreach (var user in dataBase)
+                {
+                    LoginAndPassword = user.Split(" ");
+                    if (LoginAndPassword[0].CompareTo(LoginTextBox.Text) == 0 && LoginAndPassword[1].CompareTo(PasswordTextBox.Password) == 0)
+                    {
+                        Page2 page2 = new Page2(); // Create an instance of Page2
+                        NavigationService.Navigate(page2, LoginAndPassword[0]);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        private void SignUpButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            File.WriteAllText("database.txt", LoginTextBox.Text + " " + PasswordTextBox.Password + "\n");
+            Page2 page2 = new Page2(); // Create an instance of Page2
+            NavigationService.Navigate(page2, LoginTextBox.Text);
+        }
+
+        bool isSignedUp(string userName)
+        {
+            //open file and search for username
+
+            return false;
+        }
+    }
+}
---
+++ Page2.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace forumcsharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        string login;
+        private string IP = "127.0.0.1";
+        private int Port = 27015;
+
+        public Page2()
+        {
+            InitializeComponent();
+            //Show();
+            if (!setupSocket(IP, Port))
+            {
+                MessageBox.Show("Something went wrong connecting to a server!");
+                //Close();
+            }
+
+            Thread listenerThread = new Thread(listenAndPrintMessages);
+            listenerThread.Start();
+
+        }
+
+        private TcpClient client;
+        private bool setupSocket(string IP, int Port)
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect(IP, Port);
+                ForumWindow.Items.Add($"Connected to the server on {IP}:{Port}");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ForumWindow.Items.Add($"Error connecting to the server on {IP}:{Port}");
+                return false;
+            }
+        }
+
+        private bool sendMessage(string message)
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                Array.Resize(ref data, data.Length + 1);
+                data[data.Length - 1] = (byte)'\0';
+
+                stream.Write(data, 0, data.Length);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ForumWindow.Items.Add($"Error sending message!");
+                return false;
+            }
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            string messageToSend = ForumTextBox.Text;
+            if (messageToSend.Length > 0)
+                if (!sendMessage(messageToSend))
+                    MessageBox.Show("Message has not been sent!");
+
+            ForumTextBox.Text = "";
+        }
+
+        private void updateForumChat(string message)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                ForumWindow.Items.Add(message);
+            });
+        }
+
+        private void listenAndPrintMessages()
+        {
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead > 0)
+                {
+                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    updateForumChat(receivedData);
+                }
+            }
+        }
+
+        private void ForumTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendButton_Click(sender, e);
+            }
+        }
+
+        private void Navigation(object sender, NavigationEventArgs e)
+        {
+            login = (string)e.ExtraData;
+        }
+    }
+}
